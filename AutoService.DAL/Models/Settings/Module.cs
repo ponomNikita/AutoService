65c0fa85ecc946abe55bca0c784dde3e9296ae54
Nikita Ponomarev <ponom.nikita@gmail.com>
2016-10-10 14:08:25 +0300

    Commit

---
+++ AutoService.DAL/Models/Settings/Module.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoService.DAL.Models
+{
+    [Table("Module", Schema = "settings")]
+    class Module
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
