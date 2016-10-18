a6342c14ac9e7e561fe8678a2b7c5b4bee6c0cdf
Пономарев Никита <nikita.ponomarev@norbit.ru>
2016-10-18 12:18:16 +0300

    Просмотр списка заявок #2

---
+++ AutoService/ViewModels/Application/ApplicationContent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoService.DAL.FilterModel;
+
+namespace AutoService.ViewModels.Application
+{
+    public class ApplicationContent
+    {
+        public ApplicationFilter Filter { get; set; }
+        public IEnumerable<AutoService.DAL.Models.Application> Items { get; set; }
+    }
+}
